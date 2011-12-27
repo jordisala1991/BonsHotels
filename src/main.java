1b03fb6f1580c11fa107f75c4683f92f3e396cfc
Pablo D. Gomez <pdgomezsol@gmail.com>
2011-12-27 11:21:21 +0100

    Prueba de commit

--- src/main.java
+++ src/main.java
@@ -6,7 +6,7 @@ public class main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo!");
+		System.out.println("Hola World!");
 	}
 
 }
