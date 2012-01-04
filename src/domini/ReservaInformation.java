b49d9f66c17f3d068b61f53db44f9127791b0ee1
Jordi <jordisala1991@gmail.com>
2012-01-04 18:36:10 +0100

    Implementada la classe ReservaInformation

---
+++ src/domini/ReservaInformation.java
@@ -0,0 +1,117 @@
+package domini;
+
+import java.util.Date;
+
+public class ReservaInformation {
+	
+	private String pob;
+	private Date dIni;
+	private Date dFi;
+	private String hotel;
+	private String tipusHab; 
+	private String dni;
+	private String nomClient;
+	private String cognoms;
+	private String email;
+	float preuTotal;
+	
+	public ReservaInformation() {
+		
+	}
+	
+	public ReservaInformation(String pob, Date dIni, Date dFi, String hotel,
+			String tipusHab, String dni, String nomClient, String cognoms,
+			String email, float preuTotal) {
+		this.pob = pob;
+		this.dIni = dIni;
+		this.dFi = dFi;
+		this.hotel = hotel;
+		this.tipusHab = tipusHab;
+		this.dni = dni;
+		this.nomClient = nomClient;
+		this.cognoms = cognoms;
+		this.email = email;
+		this.preuTotal = preuTotal;
+	}
+
+	public String getPob() {
+		return pob;
+	}
+	
+	public void setPob(String pob) {
+		this.pob = pob;
+	}
+	
+	public Date getdIni() {
+		return dIni;
+	}
+	
+	public void setdIni(Date dIni) {
+		this.dIni = dIni;
+	}
+	
+	public Date getdFi() {
+		return dFi;
+	}
+	
+	public void setdFi(Date dFi) {
+		this.dFi = dFi;
+	}
+	
+	public String getHotel() {
+		return hotel;
+	}
+	
+	public void setHotel(String hotel) {
+		this.hotel = hotel;
+	}
+	
+	public String getTipusHab() {
+		return tipusHab;
+	}
+	
+	public void setTipusHab(String tipusHab) {
+		this.tipusHab = tipusHab;
+	}
+	
+	public String getDni() {
+		return dni;
+	}
+	
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	
+	public String getNomClient() {
+		return nomClient;
+	}
+	
+	public void setNomClient(String nomClient) {
+		this.nomClient = nomClient;
+	}
+	
+	public String getCognoms() {
+		return cognoms;
+	}
+	
+	public void setCognoms(String cognoms) {
+		this.cognoms = cognoms;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public float getPreuTotal() {
+		return preuTotal;
+	}
+	
+	public void setPreuTotal(float preuTotal) {
+		this.preuTotal = preuTotal;
+	}
+	
+}
