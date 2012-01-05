74b34744ebd7afbc0294afb7af7f8404ac09ea0e
Dani Llamazares <danielcrack69@gmail.com>
2012-01-05 17:59:38 +0100

    HotelInformation implemented.
    It's an awesome chance because it didn't exist.

---
+++ src/domini/HotelInformation.java
@@ -0,0 +1,65 @@
+package domini;
+
+import java.util.Date;
+import java.util.HashSet;
+
+public class HotelInformation {
+	
+	private String nomHotel;
+	private String desc;
+	private String categoria;
+	private float avaluacio;
+	private HashSet<TipusHabInformation> habs;
+	
+	public HotelInformation() {
+		
+	}
+	
+	public HotelInformation(String nomHotel, String desc, String categoria, float avaluacio, HashSet<TipusHabInformation> habs) {
+		this.nomHotel = nomHotel;
+		this.desc = desc;
+		this.categoria = categoria;
+		this.avaluacio = avaluacio;
+		this.habs = habs;
+	}
+
+	public String getNomHotel() {
+		return nomHotel;
+	}
+	
+	public void setNomHotel(String nomHotel) {
+		this.nomHotel = nomHotel;
+	}
+	
+	public String getDesc() {
+		return desc;
+	}
+	
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+	
+	public float getAvaluacio() {
+		return avaluacio;
+	}
+	
+	public void setAvaluacio(float avaluacio) {
+		this.avaluacio = avaluacio;
+	}
+	
+	public HashSet<TipusHabInformation> getHabs() {
+		return habs;
+	}
+	
+	public void setHabs(HashSet<TipusHabInformation> habs) {
+		this.habs = habs;
+	}
+}
