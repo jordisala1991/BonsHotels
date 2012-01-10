5d0c84c1c50f4fbadf17bb24f6415d185f588ee3
Jordi <jordisala1991@gmail.com>
2012-01-10 16:08:04 +0100

    View1 Implementend

---
+++ src/presentacio/View1.java
@@ -0,0 +1,71 @@
+package presentacio;
+
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+
+public class View1 extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField areaMessage;
+	private ControladorVistaReservarHabitacio controladorVistaReservarHabitacio;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View1 frame = new View1();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public View1() {
+		controladorVistaReservarHabitacio = new ControladorVistaReservarHabitacio();
+		setTitle("BonsHotels");
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 250, 120);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton reservarHabitacio = new JButton("Reservar Habitació");
+		reservarHabitacio.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				controladorVistaReservarHabitacio.PrReservarHabitacio();
+			}
+		});
+		reservarHabitacio.setBounds(50, 11, 150, 30);
+		contentPane.add(reservarHabitacio);
+		
+		areaMessage = new JTextField();
+		areaMessage.setText("Area Message");
+		areaMessage.setHorizontalAlignment(SwingConstants.CENTER);
+		areaMessage.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		areaMessage.setEditable(false);
+		areaMessage.setBounds(10, 61, 224, 20);
+		contentPane.add(areaMessage);
+		areaMessage.setColumns(10);
+	}
+}
