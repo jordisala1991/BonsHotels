0f7cdc00c3cca400b59e580c2dc785e59ada8fd2
Jordi <jordisala1991@gmail.com>
2012-01-10 16:09:17 +0100

    Fixed Some errors on ControladorVistaReservarHabitacio Now controls empty fields

--- src/presentacio/ControladorVistaReservarHabitacio.java
+++ src/presentacio/ControladorVistaReservarHabitacio.java
@@ -30,8 +30,10 @@ public class ControladorVistaReservarHabitacio {
 		}
 	}
 	
-	public void PrOKObteHotels(String pob, String sIni, String sFi, int numOcup) {
+	public void PrOKObteHotels(String pob, String sIni, String sFi, String sNumOcup) {
 		try {
+			if (pob.equals(null) || sIni.equals("") || sFi.equals("") || sNumOcup.equals("")) 
+				throw new Exception ("faltenDades");
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
 			sdf.setLenient(false);
 			Date dIni, dFi;
@@ -43,11 +45,17 @@ public class ControladorVistaReservarHabitacio {
 			} catch (ParseException pe) { throw new Exception ("sFiNoValida"); }
 			if (!dIni.after(new Date())) throw new Exception ("dIniNoPosteriorAvui");
 			if (!dFi.after(dIni)) throw new Exception ("dFiNoPosteriordIni");
+			int numOcup;
+			try {
+				numOcup = Integer.parseInt(sNumOcup);
+			} catch (NumberFormatException nfe) { throw new Exception ("sNumOcupNoValid"); }
 			if (numOcup <= 0) throw new Exception ("numOcupNoOk");
 			HotelInformation hotels = controladorReservarHabitacio.buscarHabitacio(dIni, dFi, numOcup);
 			vistaReservarHabitacio.mostraHotels(hotels);
 		} catch (Exception e) {
 			switch (e.getMessage()) {
+				case "faltenDades":
+					vistaReservarHabitacio.mostraMissatge("S'han d'introduir totes les dades");
 				case "sIniNoValida":
 					vistaReservarHabitacio.mostraMissatge("La data inici no és una data vàlida");
 				case "sFiNoValida":
@@ -56,6 +64,8 @@ public class ControladorVistaReservarHabitacio {
 					vistaReservarHabitacio.mostraMissatge("La data inici no és posterior a la data d'avui");
 				case "dFiNoPosteriordIni":
 					vistaReservarHabitacio.mostraMissatge("La data fi no és posterior a la data inici");
+				case "sNumOcupNoValid":
+					vistaReservarHabitacio.mostraMissatge("El nombre d'ocupants ha de ser un número");
 				case "numOcupNoOk":
 					vistaReservarHabitacio.mostraMissatge("El nombre d'ocupants ha de ser superior a 0");
 				case "hotelsNoDisp":
@@ -65,33 +75,47 @@ public class ControladorVistaReservarHabitacio {
 	}
 	
 	public void PrOKSeleccionarHabitacio(String hotel, String tipusHab) {
-		ReservaInformation res = controladorReservarHabitacio.seleccionarHabitacio(hotel, tipusHab);
-		vistaReservarHabitacio.mostraPreu(res);
+		try {
+			if (hotel.equals(null) || tipusHab.equals(null)) throw new Exception ("faltenDades");
+			ReservaInformation res = controladorReservarHabitacio.seleccionarHabitacio(hotel, tipusHab);
+			vistaReservarHabitacio.mostraPreu(res);
+		} catch (Exception e) {
+			if (e.getMessage().equals("faltenDades")) 
+				vistaReservarHabitacio.mostraMissatge("S'han d'introduir totes les dades");
+		}
 	}
 	
 	public void PrOKObteDadesClient(String dni) {
 		try {
+			if (dni.equals("")) throw new Exception ("faltenDades");
 			ReservaInformation dades = controladorReservarHabitacio.introduirDni(dni);
 			vistaReservarHabitacio.mostraDadesClient(dades);
 		} catch (Exception e) {
-			if (e.getMessage().equals("clientNoExisteix"))
-				vistaReservarHabitacio.mostraMissatge("El client no existeix");
+			switch (e.getMessage()) {
+				case "faltenDades":
+					vistaReservarHabitacio.mostraMissatge("S'han d'introduir totes les dades");
+				case "clientNoExisteix":
+					vistaReservarHabitacio.mostraMissatge("El client no existeix");
+			}
 		}
 	}
 	
 	public void PrOKReservaCreada(String numTarg, String cCad) {
 		try {
+			if (numTarg.equals("") || cCad.equals("")) throw new Exception ("faltenDades");
 			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
 			sdf.setLenient(false);
 			Date dCad;
 			try {
-				dCad = sdf.parse(cCad);
-			} catch (ParseException pe) { throw new Exception ("cCadNoValida"); }
+				dCad = sdf.parse(sCad);
+			} catch (ParseException pe) { throw new Exception ("sCadNoValida"); }
 			controladorReservarHabitacio.pagament(numTarg, dCad);
 			vistaReservarHabitacio.mostraMissatgeFi();
 		} catch (Exception e) {
 			switch (e.getMessage()) {
-				case "cCadNoValida":
+				case "faltenDades":
+					vistaReservarHabitacio.mostraMissatge("S'han d'introduir totes les dades");
+				case "sCadNoValida":
 					vistaReservarHabitacio.mostraMissatge("La data de caducitat no és una data vàlida");
 				case "serveiNoDisponible":
 					vistaReservarHabitacio.mostraMissatge("El servei no esta disponible");
